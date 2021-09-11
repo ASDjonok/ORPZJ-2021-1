108dd3d685aac9da05efaabc660e63f052fc97ef
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-09-11 09:02:04 +0300

    fix formatting of lab1 by Liza Dolgova

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import ip94.dolgova.Lab1;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! In test!");
+
+//        Lab1 lab1 = new Lab1();
+        Lab1.main(new String[]{"1", "2", "4"});
     }
 }
--- src/ip94/dolgova/Lab1.java
+++ src/ip94/dolgova/Lab1.java
@@ -4,31 +4,30 @@ public class Lab1 {
 
     public static void main(String[] args) {
 
-	// Boundaries of the sum
-	int a = 2;
-	int b = -5;
-	int m = 10;
-	int n = 15;
+        // Boundaries of the sum
+        int a = 2;
+        int b = -5;
+        int m = 10;
+        int n = 15;
 
-	// Constant found by dividing the variant number
-	final int C = 9410 % 3;
+        // Constant found by dividing the variant number
+        final int C = 9410 % 3;
 
-	// Result
-	float s = 0;
+        // Result
+        float s = 0;
 
-	if (n < a || m < b) {
-	    System.out.println("S = " + s);
-	} else if (a > -C || n < -C) {
-
-	    for (int i = a; i <= n; i++) {
-		int signifier = i + C;
-		for (int j = b; j <= m; j++) {
-		    s += (float) (i * j) / signifier;
-		}
-	    }
-	    System.out.println("S = " + s);
-	} else {
-	    System.out.println("Error. Zero division is not allowed");
-	}
+        if (n < a || m < b) {
+            System.out.println("S = " + s);
+        } else if (a > -C || n < -C) {
+            for (int i = a; i <= n; i++) {
+                int signifier = i + C;
+                for (int j = b; j <= m; j++) {
+                    s += (float) (i * j) / signifier;
+                }
+            }
+            System.out.println("S = " + s);
+        } else {
+            System.out.println("Error. Zero division is not allowed");
+        }
     }
 }

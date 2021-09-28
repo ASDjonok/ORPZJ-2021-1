c741775aa6dd72fc4b8ba0c6e7bd3ab8262fd69b
UlianaDzigil <68972045+UlianaDzigil@users.noreply.github.com>
2021-09-28 18:07:36 +0300

    Update Main.java

--- Lab3.1.1/src/com/company/Main.java
+++ Lab3.1.1/src/com/company/Main.java
@@ -8,40 +8,34 @@ public class Main {
         //Операция О2 +
         //Константа 0
         //Тип индексов i и j short
-
         // (i + j) / (i + 0)
-
-        short a = 4;
-        short b = -10;
-        short n = 10;
-        short m = -2;
+        final short a = 4;
+        final short b = -10;
+        final short n = 10;
+        final short m = -2;
         final short C = 0; //константа
-
         float result = 0; //float по условию
 
-        if(n < a || m < b){
+        if (n < a || m < b) {
             System.out.println("Неправильные параметры сумы ");
-        }else{
-
+        } else {
             boolean isCountable = true;
-            mainLoop:
-            for(short i = a; i <= n; i++){
-                if(i + C == 0){
+
+            for (short i = a; i <= n; i++) {
+                if (i + C == 0) {
                     System.out.println("Ноль в знаменателе");
                     isCountable = false; //ставим метку что не возможно посчитать сумму
-                    break mainLoop; //если знаменатель равен 0 выходим из циклов
+                    break; //если знаменатель равен 0 выходим из циклов
                 }
                 short znam = (short) (i + C);
-
-                for(short j = b; j <= m; j++){
-
-                    result += (i + j) / znam;
+                for (short j = b; j <= m; j++) {
+                    result += (float) (i + j) / znam;
                 }
             }
-            if(isCountable == true) {
+
+            if (isCountable == true) {
                 System.out.println("Результат " + result);
             }
         }
-
     }
 }

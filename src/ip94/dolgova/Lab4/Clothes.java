69e7c39923d0077ec37204efca557a5443286181
lizzochek <littled685@gmail.com>
2021-10-07 12:12:39 +0300

    Class documentation added

--- src/ip94/dolgova/Lab4/Clothes.java
+++ src/ip94/dolgova/Lab4/Clothes.java
@@ -1,5 +1,11 @@
 package ip94.dolgova.Lab4;
 
+/**
+ * Clothes class
+ * 
+ * @autor Yelyzaveta Dolhova
+ * @version 1.0
+ */
 public class Clothes {
     String name;
     String color;
@@ -7,6 +13,15 @@ public class Clothes {
     int length;
     int width;
 
+    /**
+     * Constructor - creating a new object with certain parameters
+     *
+     * @param n name
+     * @param c color
+     * @param s size
+     * @param l length
+     * @param w width
+     */
     Clothes(String n, String c, int s, int l, int w) {
 	name = n;
 	color = c;
@@ -15,14 +30,23 @@ public class Clothes {
 	width = w;
     }
 
+    /**
+     * String representation of an object
+     */
     public String toString() {
 	return "Name: " + name + ", color: " + color + ", size: " + size + ", length: " + length + ", width: " + width;
     }
 
+    /**
+     * Getting the name value
+     */
     String getName() {
 	return name;
     }
 
+    /**
+     * Getting the length value
+     */
     int getLength() {
 	return length;
     }

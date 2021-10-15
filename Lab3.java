cd8b50e74dfd5e1eff1429d1f103e04f087e7d64
stacyy713 <71102900+stacyy713@users.noreply.github.com>
2021-10-15 19:52:54 +0300

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -4,7 +4,8 @@ import java.util.StringTokenizer;
 public class Lab3 {
     public static void main(String[] args) {
 
-        StringBuffer text = new StringBuffer("Три осенних месяца совершенно не похожи друг на друга . Сентябрь – теплый и красивый , октябрь – прохладный и дождливый , а ноябрь – морозный и холодный . Но все они несут нам что-то новое . Мне нравится каждый из них по-своему . Именно осень дарит нам бабье лето и проливные грозы , первые морозы и последнюю теплоту .");
+        //StringBuffer text = new StringBuffer("Три осенних месяца совершенно не похожи друг на друга . Сентябрь – теплый и красивый , октябрь – прохладный и дождливый , а ноябрь – морозный и холодный . Но все они несут нам что-то новое . Мне нравится каждый из них по-своему . Именно осень дарит нам бабье лето и проливные грозы , первые морозы и последнюю теплоту .");
+        StringBuffer text = new StringBuffer("Почему прогаа! не удаляет текстт?");
         System.out.println(text);
 
         StringTokenizer st = new StringTokenizer(text.toString(), " ");
@@ -21,7 +22,12 @@ public class Lab3 {
             System.out.println(li);
         }
 
-        System.out.println("----------------------");
+//        for(int i = 0; i < list.size(); i++) {
+//            if(list.get(i).contains("?") || list.get(i).contains(",") || list.get(i).contains(".") || list.get(i).contains(":") || list.get(i).contains(";")){
+//                System.out.println(list.get(i));
+//            }
+//        }
+//        System.out.println("----------------------");
 
         StringBuffer sb = new StringBuffer();
 
@@ -32,13 +38,12 @@ public class Lab3 {
 
         int len = 7;
         for (int i = 0; i < list.size(); i++) {
-            if (!isConsonant(list.get(i)) || (list.get(i).length() != len)) {
-                sb.append(list.get(i)).append(" ");
+            if (!isConsonant(list.get(i)) || (list.get(i).length() != len || list.get(i).contains("[^0-9]"))) {
+                    sb.append(list.get(i)).append(" ");
             }
         }
         System.out.println(sb);
     }
-
 
     public static boolean isConsonant(String incomingText) {
         switch (Character.toLowerCase(incomingText.charAt(0))) {
